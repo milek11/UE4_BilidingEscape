b7d08a398acd4d1ff83ab0b540ce37bc0fb3270c
Kamil <milek111@yahoo.com>
2017-09-20 06:49:51 +0100

    BE02 - fixed errors

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -10,6 +11,8 @@ UPositionReport::UPositionReport()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
+
+	
 	// ...
 }
 
@@ -19,6 +22,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetActorLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	// ...
 	
 }
